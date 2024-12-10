a4933bf03ad2189cb1ee646741ea0dda0059bc85
Vin Is A Dev <vinfehring@gmail.com>
2024-12-10 12:27:49 -0600

    Add menu functionality, world, player control

--- plague-legacy/scripts/menus/main_menu.gd
+++ plague-legacy/scripts/menus/main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_play_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/game/world/world.tscn")
 
 
 func _on_quit_button_pressed() -> void:
